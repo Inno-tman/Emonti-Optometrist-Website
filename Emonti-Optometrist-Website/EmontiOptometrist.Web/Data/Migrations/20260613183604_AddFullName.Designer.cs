257867f560cf3054d15a84f27bad4e64b6602f61
Your Name <you@example.com>
2026-06-13 20:37:52 +0200

    Show user FullName instead of email in navbar, add FullName to registration

--- Emonti-Optometrist-Website/EmontiOptometrist.Web/Data/Migrations/20260613183604_AddFullName.Designer.cs
+++ Emonti-Optometrist-Website/EmontiOptometrist.Web/Data/Migrations/20260613183604_AddFullName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EmontiOptometrist.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmontiOptometrist.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260613183604_AddFullName")]
+    partial class AddFullName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.26");
