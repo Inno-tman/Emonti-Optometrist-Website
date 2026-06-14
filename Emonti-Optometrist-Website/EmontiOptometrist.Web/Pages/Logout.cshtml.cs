6d2d8f45b6d0be16a9ae6d05c5099ffc10481244
Your Name <you@example.com>
2026-06-14 14:38:01 +0200

    Update Last_Login on customer logout

--- Emonti-Optometrist-Website/EmontiOptometrist.Web/Pages/Logout.cshtml.cs
+++ Emonti-Optometrist-Website/EmontiOptometrist.Web/Pages/Logout.cshtml.cs
@@ -1,13 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
 using EmontiOptometrist.Web.Services;
 
 namespace EmontiOptometrist.Web.Pages;
 
 public class LogoutModel : PageModel
 {
+    private readonly IConfiguration _configuration;
+
+    public LogoutModel(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     public IActionResult OnGet()
     {
+        var custId = AuthSession.GetCustId(HttpContext);
+        if (!string.IsNullOrEmpty(custId))
+        {
+            string connStr = _configuration.GetConnectionString("DefaultConnection") ?? "DataSource=app.db;Cache=Shared";
+            using var conn = new SqliteConnection(connStr);
+            conn.Open();
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = "UPDATE customer SET Last_Login = datetime('now', 'localtime') WHERE Cust_ID = @CustId";
+            cmd.Parameters.AddWithValue("@CustId", custId);
+            cmd.ExecuteNonQuery();
+        }
+
         AuthSession.SignOut(HttpContext);
         return RedirectToPage("/Index");
     }
