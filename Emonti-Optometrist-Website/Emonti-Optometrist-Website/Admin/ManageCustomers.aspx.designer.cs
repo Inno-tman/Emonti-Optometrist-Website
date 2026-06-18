5a881880d03ceca7bd8ed64360c5f39790189279
Your Name <you@example.com>
2026-06-18 21:14:46 +0200

    Fix: add Admin pages to csproj + update designer files for Master page usage

---
+++ Emonti-Optometrist-Website/Emonti-Optometrist-Website/Admin/ManageCustomers.aspx.designer.cs
@@ -0,0 +1,9 @@
+namespace Emonti_Optometrist_Website.Admin
+{
+    public partial class ManageCustomers
+    {
+        protected global::System.Web.UI.WebControls.TextBox txtSearch;
+        protected global::System.Web.UI.WebControls.LinkButton btnSearch;
+        protected global::System.Web.UI.WebControls.GridView gvCustomers;
+    }
+}
